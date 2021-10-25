a93fc79101a2d2333d0aead98b76250d2efc1858
Vienne Kayette Javier <viennekayetteljavier@iskolarngbayan.pup.edu.ph>
2021-10-25 18:50:46 +0800

    APPLE-ORANGE_3RD COMMIT
    Created the output for the total price of apples and oranges purchased by the user.

--- Assignment 2_APPLE-ORANGE.py
+++ Assignment 2_APPLE-ORANGE.py
@@ -6,3 +6,6 @@ appleAmount=int(input("How many apples do you want to purchase? "))
 orangeAmount=int(input("How many oranges do you want to purchase? "))
 appleTotal=applePrice*appleAmount
 orangeTotal=orangePrice*orangeAmount
+
+total=appleTotal+orangeTotal
+print(f"The total amount is {total} pesos.")
