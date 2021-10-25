5532d050b6b0a76e65c4af7735225266e900e2b2
Vienne Kayette Javier <viennekayetteljavier@iskolarngbayan.pup.edu.ph>
2021-10-25 19:05:46 +0800

    APPLE-PRICE_2ND COMMIT
    This commit created the calculations for the amount of apples the user can buy with their money and their change. It also created the outputs depending on the number of apples the user can buy.

--- Assignment 2_APPLE-PRICE.py
+++ Assignment 2_APPLE-PRICE.py
@@ -1,2 +1,8 @@
 myMoney=int(input("Enter the amount of money you have: "))
 applePrice=int(input("Enter your desired price for one apple: "))
+maxApple=int(myMoney/applePrice)
+changeAmount=myMoney-maxApple*applePrice
+neededAmount=applePrice-myMoney
+if maxApple > 1: print(f"You can buy {maxApple} apples and your change is {changeAmount} pesos.")
+if maxApple == 1: print(f"You can buy {maxApple} apple and your change is {changeAmount} pesos.")
+if maxApple <= 0: print(f"You cannot buy any apples. You need {neededAmount} more pesos to complete a purchase.")
