6a73a1be582048403019d00bcae5928bdc1e097d
Vienne Kayette Javier <viennekayetteljavier@iskolarngbayan.pup.edu.ph>
2021-10-25 18:31:51 +0800

    NAME-AGE-ADDRESS_3RD COMMIT
    The last commit where I created the output of the user's inputs.

--- Assignment 2_NAME-AGE-ADDRESS.py
+++ Assignment 2_NAME-AGE-ADDRESS.py
@@ -1,3 +1,4 @@
 name=input("Enter your name: ")
 age=int(input("Enter your age: "))
 address=input("Enter your address: ")
+print(f"Hi, my name is {name}. I am {age} years old and I live in {address}.")
