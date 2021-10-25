468571449161b2960c7b3cc162d2f9f026754486
Vienne Kayette Javier <viennekayetteljavier@iskolarngbayan.pup.edu.ph>
2021-10-25 20:29:55 +0800

    APPLE-PRICE_3RD COMMIT
    Introduced floats as well as minor adjustments to make the interface better

--- Assignment 2_APPLE-PRICE.py
+++ Assignment 2_APPLE-PRICE.py
@@ -1,8 +1,8 @@
-myMoney=int(input("Enter the amount of money you have: "))
-applePrice=int(input("Enter your desired price for one apple: "))
+myMoney=float(input("\nEnter the amount of money you have: \n= "))
+applePrice=float(input("\nEnter your desired price for one apple: \n= "))
 maxApple=int(myMoney/applePrice)
-changeAmount=myMoney-maxApple*applePrice
-neededAmount=applePrice-myMoney
-if maxApple > 1: print(f"You can buy {maxApple} apples and your change is {changeAmount} pesos.")
-if maxApple == 1: print(f"You can buy {maxApple} apple and your change is {changeAmount} pesos.")
-if maxApple <= 0: print(f"You cannot buy any apples. You need {neededAmount} more pesos to complete a purchase.")
+changeAmount=float(myMoney-maxApple*applePrice)
+neededAmount=float(applePrice-myMoney)
+if  maxApple > 1: print(f'\nYou can buy {maxApple} apples and your change is {changeAmount:.2f} pesos.\n')
+elif maxApple == 1: print(f"\nYou can buy {maxApple} apple and your change is {changeAmount:.2f} pesos.\n")
+else: print(f"\nYou cannot buy any apples. You need {neededAmount:.2f} more pesos to complete a purchase.\n")
