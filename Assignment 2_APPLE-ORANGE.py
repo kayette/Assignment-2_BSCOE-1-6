16d742f4ff5e54739157e4ced9001dceee3097b4
Vienne Kayette Javier <viennekayetteljavier@iskolarngbayan.pup.edu.ph>
2021-10-25 18:49:03 +0800

    APPLE-ORANGE_2ND COMMIT
    Created inputs for the user's desired amount of oranges and apples to buy. Also created the calculations of the separate prices of apple and oranges.

--- Assignment 2_APPLE-ORANGE.py
+++ Assignment 2_APPLE-ORANGE.py
@@ -1,3 +1,8 @@
 applePrice=int(20)
 orangePrice=int(25)
 print("Hi there, welcome to our store! An apple costs 20 pesos, while an orange costs 25 pesos.")
+
+appleAmount=int(input("How many apples do you want to purchase? "))
+orangeAmount=int(input("How many oranges do you want to purchase? "))
+appleTotal=applePrice*appleAmount
+orangeTotal=orangePrice*orangeAmount
